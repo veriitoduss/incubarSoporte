65611a35e538319580f6e35c4da741809e7463a0
Veronica <vejumadu@gmail.com>
2022-10-03 08:25:38 -0500

    prueba1

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+require_once("Config/config.php");
+require_once("Helpers/helpers.php");
+$url = !empty($_GET['url']) ? $_GET['url'] : 'home/home';
+$arrUrl = explode("/", $url);
+$controller = $arrUrl[0];
+$method = $arrUrl[0];
+$params = "";
+
+if (!empty($arrUrl[1])) {
+  if ($arrUrl[1] != "") {
+    $method = $arrUrl[1];
+  }
+}
+if (!empty($arrUrl[2])) {
+  if ($arrUrl[2] != "") {
+    for ($i = 2; $i < count($arrUrl); $i++) {
+      $params .= $arrUrl[$i] . ',';
+    }
+    $params = trim($params, ',');
+  }
+}
+require_once ("libraries/Core/AutoLoad.php");
+require_once ("libraries/Core/Load.php");
+?>
